68c8627090dab38494b27ebe9be9d3b880b18095
Gabe Mercer <Gabe Mercer>
2016-09-23 13:16:19 -0400

    updated website structue

--- js/js.js
+++ js/js.js
@@ -16,31 +16,55 @@ $(document).ready(function(){
 		$(".fadein").hide();
 		$("#con-id-frm").fadeIn("slow");
 		
-	})
+	});
 	
 	$("#base-rate").on("click", function(){
 		$(".fadein").hide();
 		$("#bs-rt-frm").fadeIn("slow");
 		
-	})
+	});
 	
 	$("#dt-crt").on("click", function(){
 		$(".fadein").hide();
 		$("#dt-crt-frm").fadeIn("slow");
 		
-	})
+	});
 	
 	$("#pd-dt").on("click", function(){
 		$(".fadein").hide();
 		$("#pd-dt-frm").fadeIn("slow");
 		
-	})
+	});
 	
 	$("#con").on("click", function(){
 		$(".fadein").hide();
 		$("#con-frm").fadeIn("slow");
 		
-	})
+	});
+	
+	//These are for the buttons in the admin home page select
+	$("#sel").on("click", function(){
+		$(".fadein").fadeOut("slow");
+		
+	});
+	
+	$("#maggie").on("click", function(){
+		$(".fadein").hide();
+		$("#u-frm").fadeIn("slow");
+		
+	});
+	
+	$("#sally").on("click", function(){
+		$(".fadein").hide();
+		$("#u-frm").fadeIn("slow");
+		
+	});
+	
+	$("#fred").on("click", function(){
+		$(".fadein").hide();
+		$("#u-frm").fadeIn("slow");
+		
+	});
 });
 
 /*note, use "change" event, */
